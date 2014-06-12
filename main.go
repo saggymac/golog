f00aab0977360f6667bb27ff25430a7ab28b8a5f
Scott A. Guyer <saguyer@scotts-mbp.home>
2014-06-11 23:52:17 -0400

    just getting started

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"html/template"
+)
+
+func loadTemplate( templateName string ) *template.Template {
+	t, _ := template.ParseFiles( "tmpl/" + templateName)
+	return t
+}
+
+
+func rootHandler( w http.ResponseWriter, r *http.Request ) {
+	tmpl := loadTemplate( "index.html")
+	if tmpl != nil {
+		tmpl.Execute( w, nil)
+	}
+}
+
+
+
+func main() {
+	http.HandleFunc( "/", rootHandler)
+	log.Fatal( http.ListenAndServe( ":8080", nil))
+}
+
+
